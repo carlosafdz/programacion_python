c11ae66dde89494184bc59b06bc8665baa76c967
Carlos Fernandez <carlos.afdzf@hotmail.com>
2020-03-20 14:32:17 -0700

    web screapper

---
+++ 21_webscrapper/web_scrapping.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+
+def run():
+    for i in range(1,6):
+        response = requests.get(f'https://xkcd.com/{i}')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        image_container = soup.find(id='comic')
+
+        image_url = image_container.find('img')['src']
+        image_name = image_url.split('/')[-1]
+
+        print('descargando la imagen')
+
+        urllib.request.urlretrieve(f'https:{image_url}',image_name)
+
+
+if __name__ == "__main__":
+    run()
